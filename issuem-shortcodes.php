01993dd86207f67d01c08f507371ed31ad2a55f6
Karl Bowden <karlbowden@gmail.com>
2013-11-13 09:37:01 +1100

    Added option for the [issuem_archives] shortcode to generate issue urls using get_term_link instead of based on the shortcode page with a backup to the previous links in the event of an error

--- issuem-shortcodes.php
+++ issuem-shortcodes.php
@@ -328,7 +328,10 @@ if ( !function_exists( 'do_issuem_archives' ) ) {
 			else
 				$article_page = get_page_link( $issuem_settings['page_for_articles'] );
 		
-			$issue_url = add_query_arg( 'issue', $issue_array[0]->slug, $article_page );
+			$issue_url = get_term_link( $issue_array[0], 'issuem_issue' );
+            if ( $issuem_settings['use_issue_tax_links'] == '' or is_wp_error( $issue_url ) ) {
+                $issue_url = add_query_arg( 'issue', $issue_array[0]->slug, $article_page );
+            }
 				
 			if ( !empty( $issue_array[1]['pdf_version'] ) || !empty( $issue_meta['external_pdf_link'] ) ) {
 				
