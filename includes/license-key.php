2a507c05cba3c6a7f2ec3f629a610edd9755c530
Jeremy Green <greenhornet79@gmail.com>
2020-11-17 06:50:33 -0700

    add license key tab and functionality

---
+++ includes/license-key.php
@@ -0,0 +1,220 @@
+<?php
+
+/**
+ * Registers IssueM License Key class
+ *
+ * @package IssueM
+ * @since 2.8.4
+ */
+
+/**
+ * This class registers the IssueM license key activate/deactive functionality
+ *
+ * @since 2.8.4
+ */
+
+class Issuem_License_Key
+{
+
+    private $plugin_slug;
+    private $plugin_prefix; // underscored version of slug
+    private $plugin_name;
+
+    /**
+     * Class constructor, puts things in motion
+     *
+     * @since 2.8.4
+     */
+    function __construct($plugin_slug, $plugin_name)
+    {
+
+        $this->plugin_slug = $plugin_slug;
+        $this->plugin_name = $plugin_name;
+        $this->plugin_prefix = str_replace('-', '_', $plugin_slug);
+
+        add_action('admin_init', array($this, 'activate_license'));
+        add_action('admin_init', array($this, 'deactivate_license'));
+
+        add_action('issuem_after_licenses_settings', array($this, 'license_key_settings_div'));
+    }
+
+    /**
+     * Get IssueM License Settings for this plugin
+     *
+     * @since 4.4.0
+     */
+    public function get_settings()
+    {
+
+        $defaults = array(
+            'license_key'                    => '',
+            'license_status'                => '',
+        );
+
+        $defaults = apply_filters($this->plugin_slug . '_default_license_settings', $defaults);
+        $settings = get_option($this->plugin_slug);
+        $settings = wp_parse_args($settings, $defaults);
+
+        return apply_filters($this->plugin_prefix . '_get_license_settings', $settings);
+    }
+
+    /**
+     * Update IssueM License Settings for this plugin
+     *
+     * @since 2.8.4
+     */
+    public function update_settings($settings)
+    {
+        update_option($this->plugin_slug, $settings);
+    }
+
+    /**
+     * Create and display license key on the IssueM settings page
+     *
+     * @since 2.8.4
+     */
+    public function license_key_settings_div()
+    {
+
+        $settings = $this->get_settings();
+
+?>
+
+        <div class="issuem-license-key-box">
+            <h3 class="issuem-license-key-title"><span><?php _e($this->plugin_name, $this->plugin_slug); ?></span></h3>
+
+            <table id="<?php echo $this->plugin_prefix; ?>_license_key" class="form-table">
+                <tr>
+                    <th rowspan="1">
+                        <?php _e('License Key', $this->plugin_slug); ?>
+                    </th>
+
+                    <td>
+                        <input type="text" id="<?php echo $this->plugin_prefix; ?>_license_key" class="regular-text" name="<?php echo $this->plugin_prefix; ?>_license_key" value="<?php echo htmlspecialchars(stripcslashes($settings['license_key'])); ?>" />
+
+                        <?php if (
+                            $settings['license_status'] !== false
+                            && $settings['license_status'] == 'valid'
+                        ) {
+                            // license is active
+                        ?>
+                            <span style="color:green;"><?php _e('active'); ?></span>
+                            <input type="submit" class="button-secondary" name="<?php echo $this->plugin_prefix; ?>_license_deactivate" value="<?php _e('Deactivate License', $this->plugin_slug); ?>" />
+
+                        <?php } else if ($settings['license_status'] == 'invalid') {
+                            // license is invalid
+                        ?>
+                            <span style="color:red;"><?php _e('invalid'); ?></span>
+                            <input type="submit" class="button-secondary" name="<?php echo $this->plugin_prefix; ?>_license_activate" value="<?php _e('Activate License', $this->plugin_slug); ?>" />
+
+                        <?php } else {
+                            // license hasn't been entered yet
+                        ?>
+                            <input type="submit" class="button-secondary" name="<?php echo $this->plugin_prefix; ?>_license_activate" value="<?php _e('Activate License', $this->plugin_slug); ?>" />
+
+                        <?php } ?>
+
+                    </td>
+                </tr>
+            </table>
+
+        </div>
+
+
+<?php
+    }
+
+    /**
+     * Activate the IssueM license if its valid for this plugin
+     *
+     * @since 2.8.4
+     */
+    function activate_license()
+    {
+
+        if (!isset($_POST[$this->plugin_prefix . '_license_activate'])) {
+            return;
+        }
+
+        if (!check_admin_referer('verify', 'issuem_license_wpnonce')) {
+            return;
+        }
+
+        $settings = $this->get_settings();
+
+        if (!empty($_POST[$this->plugin_prefix . '_license_key'])) {
+            $settings['license_key'] = $_POST[$this->plugin_prefix . '_license_key'];
+        }
+
+        $license = trim($settings['license_key']);
+
+        // data to send in our API request
+        $api_params = array(
+            'edd_action' => 'activate_license',
+            'license'     => $license,
+            'item_name' => urlencode($this->plugin_name) // the name of our product in EDD
+        );
+
+        // Call the custom API.
+        $response = wp_remote_get(esc_url_raw(add_query_arg($api_params, ZEEN101_STORE_URL)), array('timeout' => 15, 'sslverify' => false));
+
+        // make sure the response came back okay
+        if (is_wp_error($response))
+            return false;
+
+        // decode the license data
+        $license_data = json_decode(wp_remote_retrieve_body($response));
+
+        // $license_data->license will be either "active" or "inactive"
+        $settings['license_status'] = $license_data->license;
+        $this->update_settings($settings);
+    }
+
+    /**
+     * De-activate the IssueM license for this plugin
+     *
+     * @since 2.8.4
+     */
+    public function deactivate_license()
+    {
+
+        if (!isset($_POST[$this->plugin_prefix . '_license_deactivate'])) {
+            return;
+        }
+
+        if (!check_admin_referer('verify', 'issuem_license_wpnonce')) {
+            return;
+        }
+
+        $settings = $this->get_settings();
+
+        // retrieve the license from the database
+        $license = trim($settings['license_key']);
+
+        // data to send in our API request
+        $api_params = array(
+            'edd_action' => 'deactivate_license',
+            'license'     => $license,
+            'item_name' => urlencode($this->plugin_name) // the name of our product in EDD
+        );
+
+        // Call the custom API.
+        $response = wp_remote_get(esc_url_raw(add_query_arg($api_params, ZEEN101_STORE_URL)), array('timeout' => 15, 'sslverify' => false));
+
+        // make sure the response came back okay
+        if (is_wp_error($response)) {
+            return false;
+        }
+
+        // decode the license data
+        $license_data = json_decode(wp_remote_retrieve_body($response));
+
+        // $license_data->license will be either "deactivated" or "failed"
+        if ($license_data->license == 'deactivated' || $license_data->license == 'failed') {
+
+            unset($settings['license_key']);
+            unset($settings['license_status']);
+            $this->update_settings($settings);
+        }
+    }
+}
--- issuem.php
+++ issuem.php
@@ -1,10 +1,11 @@
 <?php
+
 /**
  * Main PHP file used to for initial calls to IssueM's classes and functions.
  *
  * @package IssueM
  */
- 
+
 /*
 Plugin Name: IssueM
 Plugin URI: https://zeen101.com/for-developers/issuem/
@@ -20,55 +21,56 @@ Tags: issue management, issue manager, magazine management, magazine manager, ne
  *
  * @since 1.2.0
  */
-if ( !defined( 'ZEEN101_STORE_URL' ) )
-	define( 'ZEEN101_STORE_URL', 'http://zeen101.com' );
-	
-define( 'ISSUEM_SLUG', 			'issuem' );
-define( 'ISSUEM_VERSION', 		'2.8.3' );
-define( 'ISSUEM_DB_VERSION', 	'1.0.0' );
-define( 'ISSUEM_URL', 			plugin_dir_url( __FILE__ ) );
-define( 'ISSUEM_PATH', 			plugin_dir_path( __FILE__ ) );
-define( 'ISSUEM_BASENAME', 		plugin_basename( __FILE__ ) );
-define( 'ISSUEM_REL_DIR', 		dirname( ISSUEM_BASENAME ) );
+if (!defined('ZEEN101_STORE_URL'))
+	define('ZEEN101_STORE_URL', 'https://zeen101.com');
+
+define('ISSUEM_SLUG', 			'issuem');
+define('ISSUEM_VERSION', 		'2.8.3');
+define('ISSUEM_DB_VERSION', 	'1.0.0');
+define('ISSUEM_URL', 			plugin_dir_url(__FILE__));
+define('ISSUEM_PATH', 			plugin_dir_path(__FILE__));
+define('ISSUEM_BASENAME', 		plugin_basename(__FILE__));
+define('ISSUEM_REL_DIR', 		dirname(ISSUEM_BASENAME));
 
 /**
  * Instantiate IssueM class, require helper files
  *
  * @since 1.2.0
  */
-function issuem_plugins_loaded() {
+function issuem_plugins_loaded()
+{
 
-	require_once( 'issuem-class.php' );
+	require_once('issuem-class.php');
 
 	// Instantiate the Pigeon Pack class
-	if ( class_exists( 'IssueM' ) ) {
-		
+	if (class_exists('IssueM')) {
+
 		global $dl_plugin_issuem;
-		
+
 		$dl_plugin_issuem = new IssueM();
 		$issuem_settings = $dl_plugin_issuem->get_settings();
-		
-		require_once( 'issuem-post-type.php' );
-		require_once( 'issuem-taxonomy.php' );
-		require_once( 'issuem-functions.php' );
-		
-		if ( empty( $issuem_settings['use_wp_taxonomies'] ) ) { 
-			// Don't load these if we don't have too
-			require_once( 'issuem-cats-taxonomy.php' );
-			require_once( 'issuem-tags-taxonomy.php' );
-		
-		}
-		
-		require_once( 'issuem-shortcodes.php' );
-		require_once( 'issuem-widgets.php' );
-		require_once( 'issuem-feeds.php' );
-			
-		//Internationalization
-		load_plugin_textdomain( 'issuem', false, ISSUEM_REL_DIR . '/i18n/' );
-		
-		do_action( 'issuem_loaded' );
-			
-	}
 
+		require_once('issuem-post-type.php');
+		require_once('issuem-taxonomy.php');
+		require_once('issuem-functions.php');
+
+		// license key
+		include(ISSUEM_PATH . 'includes/license-key.php');
+
+		if (empty($issuem_settings['use_wp_taxonomies'])) {
+			// Don't load these if we don't have too
+			require_once('issuem-cats-taxonomy.php');
+			require_once('issuem-tags-taxonomy.php');
+		}
+
+		require_once('issuem-shortcodes.php');
+		require_once('issuem-widgets.php');
+		require_once('issuem-feeds.php');
+
+		//Internationalization
+		load_plugin_textdomain('issuem', false, ISSUEM_REL_DIR . '/i18n/');
+
+		do_action('issuem_loaded');
+	}
 }
-add_action( 'plugins_loaded', 'issuem_plugins_loaded', 4815162342 ); //wait for the plugins to be loaded before init
+add_action('plugins_loaded', 'issuem_plugins_loaded', 4815162342); //wait for the plugins to be loaded before init
